3dc4205f8640f07e9a2d36ed65e6bc39068c30aa
SebastiánEPH <sebastianeph99@gmail.com>
2020-03-09 23:54:38 -0500

    added more functions

--- Libs/Regedit.cs
+++ Libs/Regedit.cs
@@ -8,11 +8,44 @@ using System.Threading.Tasks;
 
 namespace RegistryTools.Libs {
     class Regedit {
+                            /*          0   =       °Se crea un conteiner no una llave°
+                     *          1   =       String Value
+                     *          2   =       Binarie Value
+                     *          3   =       DWORD (32bits) Value
+                     *          4   =       QWORD (64bits) Value
+                     *          5   =       Multi-String Value
+                     *          6   =       Expandable String   
+                     *          */
 
-        public void createNewRegistryKey() {        //Función crear llave de Registro
+        public void createNewRegistryKeyValue_String() {        //Función crear llave de Registro
         
         }
-        public void createNewRegistryConteiner() {  //Finción crear carpeta contenedora
+        public void createNewRegistryKeyValue_Binarie() {        //Función crear llave de Registro
+
+        }
+        public void createNewRegistryKey_DWORD() {        //Función crear llave de Registro
+
+        }
+        public void createNewRegistryKey_QWORD() {        //Función crear llave de Registro
+
+        }
+        public void createNewRegistryKey_MultiString() {        //Función crear llave de Registro
+
+        }
+        public void createNewRegistryKey_ExpandableString() {        //Función crear llave de Registro
+
+        }
+
+        public void createNewRegistryConteiner() {  //Función crear carpeta contenedora
+
+        }
+        public void createNewRegistryConteiner() {  //Función crear carpeta contenedora
+
+        }
+        public void createNewRegistryConteiner() {  //Función crear carpeta contenedora
+
+        }
+        public void createNewRegistryConteiner() {  //Función crear carpeta contenedora
 
         }
 
