7b25c4ff9d11ad2cf1046f5bc24fa29df1f78c65
SebastiánEPH <sebastianeph99@gmail.com>
2020-03-12 00:53:47 -0500

    Formulario - Create key value - beta

--- f_main.Designer.cs
+++ f_main.Designer.cs
@@ -38,12 +38,26 @@
             this.crearConteiner_Contenedor = new System.Windows.Forms.TextBox();
             this.crearConteiner_ruta = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.crearLlave = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.crearLlave_name = new System.Windows.Forms.TextBox();
+            this.crearLlave_ruta = new System.Windows.Forms.TextBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
             this.txt_info = new System.Windows.Forms.Label();
+            this.createLlave_value = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.rb_binario = new System.Windows.Forms.RadioButton();
+            this.rb_QWORD = new System.Windows.Forms.RadioButton();
+            this.rb_DWORD = new System.Windows.Forms.RadioButton();
+            this.rb_multiString = new System.Windows.Forms.RadioButton();
+            this.rb_expString = new System.Windows.Forms.RadioButton();
+            this.rb_String = new System.Windows.Forms.RadioButton();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -95,7 +109,7 @@
             // 
             // btn_CrearContenedor
             // 
-            this.btn_CrearContenedor.Location = new System.Drawing.Point(23, 110);
+            this.btn_CrearContenedor.Location = new System.Drawing.Point(15, 116);
             this.btn_CrearContenedor.Name = "btn_CrearContenedor";
             this.btn_CrearContenedor.Size = new System.Drawing.Size(324, 23);
             this.btn_CrearContenedor.TabIndex = 3;
@@ -106,7 +120,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 65);
+            this.label3.Location = new System.Drawing.Point(15, 71);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(165, 13);
             this.label3.TabIndex = 1;
@@ -115,7 +129,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 17);
+            this.label2.Location = new System.Drawing.Point(15, 23);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(77, 13);
             this.label2.TabIndex = 1;
@@ -123,20 +137,33 @@
             // 
             // crearConteiner_Contenedor
             // 
-            this.crearConteiner_Contenedor.Location = new System.Drawing.Point(23, 84);
+            this.crearConteiner_Contenedor.Location = new System.Drawing.Point(15, 90);
             this.crearConteiner_Contenedor.Name = "crearConteiner_Contenedor";
             this.crearConteiner_Contenedor.Size = new System.Drawing.Size(165, 20);
             this.crearConteiner_Contenedor.TabIndex = 0;
             // 
             // crearConteiner_ruta
             // 
-            this.crearConteiner_ruta.Location = new System.Drawing.Point(23, 36);
+            this.crearConteiner_ruta.Location = new System.Drawing.Point(15, 42);
             this.crearConteiner_ruta.Name = "crearConteiner_ruta";
             this.crearConteiner_ruta.Size = new System.Drawing.Size(324, 20);
             this.crearConteiner_ruta.TabIndex = 0;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.rb_String);
+            this.tabPage2.Controls.Add(this.rb_expString);
+            this.tabPage2.Controls.Add(this.rb_multiString);
+            this.tabPage2.Controls.Add(this.rb_DWORD);
+            this.tabPage2.Controls.Add(this.rb_QWORD);
+            this.tabPage2.Controls.Add(this.rb_binario);
+            this.tabPage2.Controls.Add(this.crearLlave);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.createLlave_value);
+            this.tabPage2.Controls.Add(this.crearLlave_name);
+            this.tabPage2.Controls.Add(this.crearLlave_ruta);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -144,6 +171,48 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Crear Llave";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // crearLlave
+            // 
+            this.crearLlave.Location = new System.Drawing.Point(200, 135);
+            this.crearLlave.Name = "crearLlave";
+            this.crearLlave.Size = new System.Drawing.Size(191, 23);
+            this.crearLlave.TabIndex = 8;
+            this.crearLlave.Text = "Ejecutar";
+            this.crearLlave.UseVisualStyleBackColor = true;
+            this.crearLlave.Click += new System.EventHandler(this.btncrearLlave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ingrese el nombre de la llave";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Ingrese la ruta:";
+            // 
+            // crearLlave_name
+            // 
+            this.crearLlave_name.Location = new System.Drawing.Point(18, 77);
+            this.crearLlave_name.Name = "crearLlave_name";
+            this.crearLlave_name.Size = new System.Drawing.Size(165, 20);
+            this.crearLlave_name.TabIndex = 4;
+            // 
+            // crearLlave_ruta
+            // 
+            this.crearLlave_ruta.Location = new System.Drawing.Point(18, 29);
+            this.crearLlave_ruta.Name = "crearLlave_ruta";
+            this.crearLlave_ruta.Size = new System.Drawing.Size(165, 20);
+            this.crearLlave_ruta.TabIndex = 5;
             // 
             // tabPage3
             // 
@@ -181,6 +250,84 @@
             this.txt_info.TabIndex = 2;
             this.txt_info.Text = "---";
             // 
+            // createLlave_value
+            // 
+            this.createLlave_value.Location = new System.Drawing.Point(18, 128);
+            this.createLlave_value.Name = "createLlave_value";
+            this.createLlave_value.Size = new System.Drawing.Size(165, 20);
+            this.createLlave_value.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 109);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Valor de la llave";
+            // 
+            // rb_binario
+            // 
+            this.rb_binario.AutoSize = true;
+            this.rb_binario.Location = new System.Drawing.Point(200, 44);
+            this.rb_binario.Name = "rb_binario";
+            this.rb_binario.Size = new System.Drawing.Size(86, 17);
+            this.rb_binario.TabIndex = 9;
+            this.rb_binario.Text = "Binario value";
+            this.rb_binario.UseVisualStyleBackColor = true;
+            // 
+            // rb_QWORD
+            // 
+            this.rb_QWORD.AutoSize = true;
+            this.rb_QWORD.Location = new System.Drawing.Point(200, 90);
+            this.rb_QWORD.Name = "rb_QWORD";
+            this.rb_QWORD.Size = new System.Drawing.Size(97, 17);
+            this.rb_QWORD.TabIndex = 10;
+            this.rb_QWORD.Text = "QWORD value";
+            this.rb_QWORD.UseVisualStyleBackColor = true;
+            // 
+            // rb_DWORD
+            // 
+            this.rb_DWORD.AutoSize = true;
+            this.rb_DWORD.Location = new System.Drawing.Point(200, 67);
+            this.rb_DWORD.Name = "rb_DWORD";
+            this.rb_DWORD.Size = new System.Drawing.Size(97, 17);
+            this.rb_DWORD.TabIndex = 11;
+            this.rb_DWORD.Text = "DWORD value";
+            this.rb_DWORD.UseVisualStyleBackColor = true;
+            // 
+            // rb_multiString
+            // 
+            this.rb_multiString.AutoSize = true;
+            this.rb_multiString.Location = new System.Drawing.Point(200, 113);
+            this.rb_multiString.Name = "rb_multiString";
+            this.rb_multiString.Size = new System.Drawing.Size(103, 17);
+            this.rb_multiString.TabIndex = 12;
+            this.rb_multiString.Text = "MultiString value";
+            this.rb_multiString.UseVisualStyleBackColor = true;
+            // 
+            // rb_expString
+            // 
+            this.rb_expString.AutoSize = true;
+            this.rb_expString.Location = new System.Drawing.Point(283, 21);
+            this.rb_expString.Name = "rb_expString";
+            this.rb_expString.Size = new System.Drawing.Size(108, 17);
+            this.rb_expString.TabIndex = 13;
+            this.rb_expString.Text = "ExpandableString";
+            this.rb_expString.UseVisualStyleBackColor = true;
+            // 
+            // rb_String
+            // 
+            this.rb_String.AutoSize = true;
+            this.rb_String.Checked = true;
+            this.rb_String.Location = new System.Drawing.Point(200, 21);
+            this.rb_String.Name = "rb_String";
+            this.rb_String.Size = new System.Drawing.Size(81, 17);
+            this.rb_String.TabIndex = 14;
+            this.rb_String.TabStop = true;
+            this.rb_String.Text = "String value";
+            this.rb_String.UseVisualStyleBackColor = true;
+            // 
             // f_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -195,6 +342,8 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,6 +364,19 @@
         private System.Windows.Forms.TextBox crearConteiner_Contenedor;
         private System.Windows.Forms.TextBox crearConteiner_ruta;
         private System.Windows.Forms.Button btn_CrearContenedor;
+        private System.Windows.Forms.Button crearLlave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox crearLlave_name;
+        private System.Windows.Forms.TextBox crearLlave_ruta;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox createLlave_value;
+        private System.Windows.Forms.RadioButton rb_String;
+        private System.Windows.Forms.RadioButton rb_expString;
+        private System.Windows.Forms.RadioButton rb_multiString;
+        private System.Windows.Forms.RadioButton rb_DWORD;
+        private System.Windows.Forms.RadioButton rb_QWORD;
+        private System.Windows.Forms.RadioButton rb_binario;
     }
 }
 
--- f_main.cs
+++ f_main.cs
@@ -23,6 +23,23 @@ namespace RegistryTools {
             // El mensaje de confirmación o de Falló se mostrará en la pantalla
             txt_info.Text = registro.createConteiner(ruta, nombre);
         }
+
+        private void btncrearLlave(object sender, EventArgs e) {
+            // Ruta completa del registro
+            //Ejemplo: @"HKEY_CURRENT_USER\Contenedor1"
+            string ruta = crearLlave_ruta.Text.ToString();
+
+            // nombre del la llave
+            string nombre = crearLlave_name.Text.ToString();
+            //Valor tipos texto
+            string valors = createLlave_value.Text.ToString();
+            //Valor tipo Entero
+            int valori = 0;
+            // El mensaje de confirmación o de Falló se mostrará en la pantalla
+            txt_info.Text = registro.createKeyValue_String(ruta, nombre, valors);
+        }
+
+     
     }
     
 }
