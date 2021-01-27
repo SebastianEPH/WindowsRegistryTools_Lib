e63341f6eb2f8b2bddb2f5ae56e36feea9dc2ac6
SebastianEPH <sebastianeph99@gmail.com>
2021-01-27 00:15:19 -0500

    added parsePath and checkpath

--- CRUD_RegistryWin_Console/Program.cs
+++ CRUD_RegistryWin_Console/Program.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CRUD_RegistryWin_Console {
-    class Program {
-        static void Main(string[] args) {
+class Program {
+    static void Main(string[] args) {
+
+        //RegistryWin hola = new RegistryWin("path");
+
+        String path = @"dsaasdsdadassdaHKEY_CURRENT_USER\SOFTWARE\BugSplat";
+        
+
+        void verifica_ruta_valida() {
+            int ixt = path.IndexOf(@"HKEY_");  // obtiene el index
+            path = path.Substring(ixt,path.Length-ixt);
+            Console.WriteLine(path);
         }
+
+
+        verifica_ruta_valida();
+        Console.ReadKey();
     }
 }
+
---
+++ RegistryWin/RegistryWin .cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;  //Librería importante 
+using System.Threading.Tasks;
+
+
+public class RegistryWin {
+
+    public string PATH = "";
+
+    public RegistryWin(string path) {
+        this.PATH = path;
+    }
+
+
+    public void createKey() {
+
+    }
+    public void deleteKey() {
+
+    }
+    public void createValue() {
+
+    }
+    public void readValue() {
+
+    }
+    public void setValue_String() {
+
+    }
+    public void setValue_Binary() {
+
+    }
+    public void setValue_DWORD() {
+
+    }
+    public void setValue_QWORD() {
+
+    }
+    public void setValue_MultiString() {
+
+    }
+    public void setValue_ExpandableString() {
+
+    }
+    public void deleteValue() {
+
+    }
+
+    private bool check_path(string path) {  // Verifica si la ruta es correcta
+        int ixt = path.IndexOf(@"HKEY_");  // obtiene el index
+        if (ixt == -1) {
+            return false;// mandar error de path incorrecto 
+            // tambien se puede mandar error de path vacio
+        } else {
+            return true;
+        }
+    }
+    private string parse_path(string path) { // Elimina el subpath de la ruta ingresada por el usuario
+        int ixt = path.IndexOf(@"HKEY_");  
+        return path.Substring(ixt,path.Length - ixt);
+    }
+
+
+}
+
